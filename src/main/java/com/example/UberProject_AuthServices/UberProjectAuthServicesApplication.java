76e6c20e4a9d6e5abc96bcdad124030c06a55115
Spaceboy789 <chintuy449@gmail.com>
2025-08-02 13:23:15 +0530

    this first commit

---
+++ src/main/java/com/example/UberProject_AuthServices/UberProjectAuthServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.UberProject_AuthServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberProjectAuthServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberProjectAuthServicesApplication.class, args);
+	}
+
+}
